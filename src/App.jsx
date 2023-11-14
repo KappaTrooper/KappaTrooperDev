e84475dd97c14aaea9076c8106553ad64e996392
Ajay Singh <contact@ajaysingh.ca>
2023-11-14 16:18:17 -0500

    Navbar setup w basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';    
 import Navbar from './components/Navbar';
-import Home from './Pages/Home';
+import Home from './pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Contact from './Pages/Contact';
 
 const App = () => {
     return (
@@ -8,10 +11,10 @@ const App = () => {
         <Router>
         <Navbar/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                {/* <Route path="/about" element={<About/>}/>
-                <Route path="/project" element={<Project/>}/>
-                <Route path="/contact" element={<Contact/>}/> */}
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/project" element={<Projects/>}/>
+                <Route path="/contact" element={<Contact/>}/>
 
             </Routes>
         </Router>
